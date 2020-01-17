bcefacdb07a0671c0f7113a4fa519fac49137322
qwert2603 <qwert2603@mail.ru>
2020-01-17 21:29:00 +0700

    allow disable indices in screenshoter

--- lib/src/screenshoter.dart
+++ lib/src/screenshoter.dart
@@ -15,10 +15,15 @@ class Screenshoter {
   final FlutterDriver _driver;
   final String _screensDir;
   final bool enabled;
+  final bool withIndices;
   int _nextScreenId = 1;
 
-  Screenshoter(this._driver, String baseScreensDir, {this.enabled: true})
-      : _screensDir = "$baseScreensDir/${DateTime.now()}" {
+  Screenshoter(
+    this._driver,
+    String baseScreensDir, {
+    this.enabled: true,
+    this.withIndices: true,
+  }) : _screensDir = "$baseScreensDir/${DateTime.now()}" {
     if (!enabled) return;
     Directory(_screensDir).createSync(recursive: true);
   }
@@ -26,7 +31,8 @@ class Screenshoter {
   Future<void> saveScreen(String name) async {
     if (!enabled) return;
     final bytes = await _driver.screenshot();
-    File("$_screensDir/${_nextScreenId++}_$name.png").writeAsBytesSync(bytes);
+    final path = "$_screensDir/${withIndices ? _nextScreenId++ : ""}_$name.png";
+    File(path).writeAsBytesSync(bytes);
   }
 
   TestAction screenshot(String name) =>
